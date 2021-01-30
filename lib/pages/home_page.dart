48999533f80d945bdee1ca0f5bc42c6b51ccb820
solutionflutter <solutionflutter@gmail.com>
2021-01-30 23:43:26 +0600

    add a bottomnavigationbar in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:social_app/widgets/variables.dart';
 
 class HomePage extends StatefulWidget {
+  customIcon(){
+    return Container(
+      width: 45,
+      height: 25,
+      child: Stack(
+        children: [
+          Container(
+
+          )
+        ],
+      )
+    );
+  }
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -8,6 +22,34 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.lightBlue,
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home,size: 25),
+            title: Text('Home',style: myStyle(12),),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search,size: 25),
+            title: Text('Search',style: myStyle(12),),
+          ),
+          BottomNavigationBarItem(
+            icon: customIcon(),
+            title: Text('',style: myStyle(12),),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message,size: 25),
+            title: Text('Message',style: myStyle(12),),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person,size: 25),
+            title: Text('Profile',style: myStyle(12),),
+          ),
+        ],
+      ),
+    );
   }
 }
