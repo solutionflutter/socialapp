d68f2ab2970ddac6f556ea0e8ba79732ef2b7ecb
solutionflutter <solutionflutter@gmail.com>
2021-01-30 16:20:39 +0600

    include the registration page

--- lib/pages/registration.dart
+++ lib/pages/registration.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:social_app/widgets/variables.dart';
 
 class RegistrationPage extends StatefulWidget {
   @override
@@ -8,6 +10,78 @@ class RegistrationPage extends StatefulWidget {
 class _RegistrationPageState extends State<RegistrationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Social App',style: myStyle(30,Colors.orangeAccent,FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Registration',style: myStyle(25,Colors.black,FontWeight.w600),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.only(left: 20,right: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                  labelStyle: myStyle(20),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  )
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.only(left: 20,right: 20),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  labelText: 'Password',
+                  icon: Icon(Icons.lock),
+                  labelStyle: myStyle(20),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text('Register',style: myStyle(20,Colors.white,FontWeight.w600),),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
