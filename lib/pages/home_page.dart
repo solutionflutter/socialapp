6479301bc1bc62c2bbc97b90222253880a7c6a14
solutionflutter <solutionflutter@gmail.com>
2021-01-31 23:18:36 +0600

    add tabs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,27 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:social_app/widgets/variables.dart';
 
 class HomePage extends StatefulWidget {
-  customIcon(){
-    return Container(
-      width: 45,
-      height: 25,
-      child: Stack(
-        children: [
-          Container(
-
-          )
-        ],
-      )
-    );
-  }
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  int page = 0;
+  customIcon(){
+    return Container(
+        width: 45,
+        height: 25,
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              width: 40,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 250, 45, 100),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 10),
+              width: 40,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 32, 210, 234),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            Center(
+              child: Container(
+                height: double.infinity,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Icon(Icons.add,size: 20),
+              )
+            )
+          ],
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.lightBlue,
         unselectedItemColor: Colors.black,
---
+++ lib/tabs/add_video.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddVideoPage extends StatefulWidget {
+  @override
+  _AddVideoPageState createState() => _AddVideoPageState();
+}
+
+class _AddVideoPageState extends State<AddVideoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
