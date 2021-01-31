4b82d95e701ae77c6db208b43c58a32fc8c31ec9
solutionflutter <solutionflutter@gmail.com>
2021-01-31 23:54:53 +0600

    add the video_page

--- lib/tabs/video_page.dart
+++ lib/tabs/video_page.dart
@@ -8,6 +8,30 @@ class VideoPage extends StatefulWidget {
 class _VideoPageState extends State<VideoPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: Colors.black
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                height: 100,
+                child: Row(
+                  children: [
+
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
